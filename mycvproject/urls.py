48a4ec514532435c348663bd69bf6c0013de7a48
Pankaj Kushwaha <lazypunky1916@gmail.com>
2024-02-16 22:50:45 +0000

    data updated

--- mycvproject/urls.py
+++ mycvproject/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("ckeditor5/", include('django_ckeditor_5.urls'), name="ck_editor_5_upload_file"),
-    path('/', include('index.urls')),
+    path('', include('index.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
