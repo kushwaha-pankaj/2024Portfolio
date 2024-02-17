fe6230da844511365dfc90b210a5fdedff3a26d4
Pankaj Kushwaha <lazypunky1916@gmail.com>
2024-02-17 14:28:24 +0000

    url changed

--- mycvproject/urls.py
+++ mycvproject/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("pankajadmin/", admin.site.urls),
     path("ckeditor5/", include('django_ckeditor_5.urls'), name="ck_editor_5_upload_file"),
     path('', include('index.urls')),
 ]
