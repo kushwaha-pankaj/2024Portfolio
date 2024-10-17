f2559cbb40f3c8e88e4465e16f6ccaa9343c9b6f
Pankaj Kushwaha <lazypunky1916@gmail.com>
2024-10-17 14:40:58 +0100

    updated for new upload

--- mycvproject/settings.py
+++ mycvproject/settings.py
@@ -14,7 +14,8 @@ SECRET_KEY = "django-insecure-3a9^)r2v25lxmmvb(5b$sz@b%c0pwh)rn=9p_rpo^uwifo%!9h
 DEBUG = True
 
 # ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['kushwahapankaj.me', 'www.kushwahapankaj.me', 'pankajportfolio.azurewebsites.net', 'www.pankajportfolio.azurewebsites.net']
+# ALLOWED_HOSTS = ['kushwahapankaj.me', 'www.kushwahapankaj.me', 'pankajportfolio.azurewebsites.net', 'www.pankajportfolio.azurewebsites.net']
+ALLOWED_HOSTS = ['127.0.0.1:8000','127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
