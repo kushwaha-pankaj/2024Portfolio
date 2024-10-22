6c996768a7987f93de5b0d6009cd59200c80cbe0
Pankaj Kushwaha <lazypunky1916@gmail.com>
2024-10-22 10:40:37 +0100

    portfolio updated

--- mycvproject/settings.py
+++ mycvproject/settings.py
@@ -14,8 +14,8 @@ SECRET_KEY = "django-insecure-3a9^)r2v25lxmmvb(5b$sz@b%c0pwh)rn=9p_rpo^uwifo%!9h
 DEBUG = True
 
 # ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['kushwahapankaj.com', 'www.kushwahapankaj.com',]
-# ALLOWED_HOSTS = ['127.0.0.1:8000','127.0.0.1']
+# ALLOWED_HOSTS = ['kushwahapankaj.com', 'www.kushwahapankaj.com',]
+ALLOWED_HOSTS = ['127.0.0.1:8000','127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
